f6c83da694ddc937a28fde51329b38e9465fa5aa
Anas Hassan <anashassan517@gmail.com>
2023-03-25 05:34:48 -0700

    final commit

--- server.py
+++ server.py
@@ -19,14 +19,14 @@ def start_server():
     client_socket, address = server_socket.accept()
     print("Connected to client at", address)
     
-    # Start the receive and send threads
+    # Start the receive and Sending threads
     receive_thread = threading.Thread(target=receive)
     receive_thread.start()
-    send_thread = threading.Thread(target=send)
-    send_thread.start()
+    Sending_thread = threading.Thread(target=Sending)
+    Sending_thread.start()
 
 def receive():
-    # Receive data from the client and sending it to the sending textbox
+    # Receive data from the client and Sendinging it to the Sendinging textbox
     while True:
         data = client_socket.recv(1024).decode("utf-8")
         if not data:
@@ -36,25 +36,25 @@ def receive():
         Recieving_textbox.insert(tk.END, data)
         Recieving_textbox.configure(state=tk.DISABLED)
 
-def send():
-    # Send data entered in the sending textbox to the client
+def Sending():
+    # Sending data entered in the Sendinging textbox to the client
     while True:
-        data = sending_queue.get()
+        data = Sendinging_queue.get()
         if data == "quit":
-            client_socket.send(data.encode("utf-8"))
+            client_socket.Sending(data.encode("utf-8"))
             client_socket.close()
             server_socket.close()
             print("Disconnected to the Client")
             break
-        client_socket.send(data.encode("utf-8"))
+        client_socket.Sending(data.encode("utf-8"))
 
-def send_message():
-    # Get the text from the sending textbox and add it to the sending queue
-    data = sending_textbox.get("1.0", "end-1c")
-    sending_queue.put(data)
+def Sending_message():
+    # Get the text from the Sendinging textbox and add it to the Sendinging queue
+    data = Sendinging_textbox.get("1.0", "end-1c")
+    Sendinging_queue.put(data)
     
-    # Clear the sending textbox
-    sending_textbox.delete("1.0", tk.END)
+    # Clear the Sendinging textbox
+    Sendinging_textbox.delete("1.0", tk.END)
 
 def close():
     # Close the client socket and server socket
@@ -81,15 +81,15 @@ port_textbox = tk.Entry(root)
 Recieving_label = tk.Label(root, text="Recieving")
 Recieving_textbox = tk.Text(root, height=6.5, width=60)
 
-# Create the sending label and text box
-sending_label = tk.Label(root, text="sending")
-sending_textbox = tk.Text(root, height=6.5, width=60)
+# Create the Sendinging label and text box
+Sendinging_label = tk.Label(root, text="Sendinging")
+Sendinging_textbox = tk.Text(root, height=6.5, width=60)
 
 # Create the start button
 start_button = tk.Button(root, text="Start Listening", command=start_server)
 
-# Create the send button
-send_button = tk.Button(root, text="Send", command=send_message)
+# Create the Sending button
+Sending_button = tk.Button(root, text="Sending", command=Sending_message)
 
 
 button = tk.Button(root, text = 'Terminate session', command = close)
@@ -101,18 +101,18 @@ port_label.grid(row=1, column=0)
 port_textbox.grid(row=1, column=1)
 Recieving_label.grid(row=2, column=0, pady=10)
 Recieving_textbox.grid(row=2, column=1, pady=10)
-sending_label.grid(row=3, column=0, pady=10)
-sending_textbox.grid(row=3, column=1, pady=10)
+Sendinging_label.grid(row=3, column=0, pady=10)
+Sendinging_textbox.grid(row=3, column=1, pady=10)
 
-# Place the start and send buttons in the last row
+# Place the start and Sending buttons in the last row
 start_button.grid(row=1, column=2, pady=10)
-send_button.grid(row=5, column=1, pady=10)
+Sending_button.grid(row=5, column=1, pady=10)
 
 # Create a socket object and set it to None
 server_socket = None
 client_socket = None
 
-# Create a queue to hold sending messages
-sending_queue = queue.Queue()
+# Create a queue to hold Sendinging messages
+Sendinging_queue = queue.Queue()
 
 root.mainloop()
